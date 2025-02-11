abd99f6b1e42da96872bfc94907e55b7ca4ccfac
DilanSriyantha <dilans091@gmail.com>
2025-02-11 06:06:07 +0530

    MUI 6 is installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,57 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
+import { Box, Button, Container, Snackbar, SnackbarCloseReason, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleClose = useCallback((
+    event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason) => {
+    if (reason == "clickaway")
+      return;
+
+    setOpen(false);
+  }, [open]);
+
+  const action = (
+    <>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth="sm">
+        <Typography variant="h4">Clinic Management System (CMS)</Typography>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h6">Initial Project Structure</Typography>
+          <Button sx={{ my: 3 }} variant="contained" onClick={handleClick}>Click Me!</Button>
+        </Box>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message={"Hola margerita!"}
+          action={action}
+        />
+      </Container>
     </>
   );
 }
