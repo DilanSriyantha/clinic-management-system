1a1e9efd488444f941c1f21b2354baa700b032eb
Sahan Diluka <sdwickkramaarachchi@gmail.com>
2025-03-02 13:28:10 +0530

    create Patient Controller and create fris of it's methods

---
+++ backend/src/main/java/org/cms/Controllers/PatientController.java
@@ -0,0 +1,75 @@
+package org.cms.Controllers;
+
+
+import org.cms.Models.Patient;
+import org.cms.Services.PatientRecords;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PatientController {
+
+    PatientRecords patientRecords = new PatientRecords();
+    private List<Patient> patientsList = patientRecords.getPatientList()+;
+    private  int firstId = 1000;
+
+    public void addPatient(int age, String name, String telephone, String email, String address, String allergiesNote, String description, Timestamp createdAt){
+        //when frist adding upadated date = created date
+        Timestamp updatedAt = createdAt;
+
+        //id creation
+        int id =+ firstId;
+        firstId++;
+
+        //refernce id nee to get
+        int referenceId = 0;
+
+        Patient patient = new Patient(id,age,name,telephone,email,address,allergiesNote,description,createdAt,updatedAt,referenceId);
+        patientsList.add(patient);
+
+        System.out.println("New patient add to system " + patient.getName()+" " + patient.getId());
+    }
+
+
+    public void removePatientByName(String name){
+        Iterator<Patient> iterator = patientsList.iterator();
+
+        while(iterator.hasNext()){
+            Patient patient = iterator.next();
+
+            if(patient.getName() == name){
+                iterator.remove();;
+                System.out.println("Patient has been removed " + patient.getName());
+                return;
+            }
+
+        }
+        System.out.println("Patient has not been found!");
+    }
+
+    public void removePatientById(int id){
+        Iterator<Patient> iterator = patientsList.iterator();
+
+        while(iterator.hasNext()){
+            Patient patient = iterator.next();
+
+            if(patient.getId() == id){
+                iterator.remove();;
+                System.out.println("Patient has been removed " + patient.getId());
+                return;
+            }
+
+        }
+        System.out.println("Patient has not been found!");
+    }
+
+//    public void updatePatient(int age, String name, String telephone, String email, String address, String allergiesNote, String description){
+//
+//        Timestamp updatedAt = new Timestamp(System.currentTimeMillis());
+//
+//
+//    }
+
+}
---
+++ backend/src/main/java/org/cms/Services/PatientRecords.java
@@ -0,0 +1,33 @@
+package org.cms.Services;
+
+import org.cms.Models.Patient;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientRecords {
+
+    private List<Patient> patientList;
+
+
+    public PatientRecords() {
+        patientList = new ArrayList<>();
+
+        patientList.add(new Patient(1, 25, "John Doe", "123-456-7890", "john.doe@example.com",
+                "123 Main St, City", "Peanuts", "Asthma patient",
+                new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), 101));
+
+        patientList.add(new Patient(2, 30, "Jane Smith", "987-654-3210", "jane.smith@example.com",
+                "456 Elm St, Town", "None", "Diabetic",
+                new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), 102));
+
+        patientList.add(new Patient(3, 40, "Michael Johnson", "555-123-4567", "michael.j@example.com",
+                "789 Oak St, Village", "Pollen", "High blood pressure",
+                new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), 103));
+    }
+
+
+    public List<Patient> getPatientList() {
+        return patientList;
+    }
+}
